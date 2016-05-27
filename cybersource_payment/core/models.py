b4aa466f3892f6f9605f7cf888a4ee400aba2a88
Khaled Sobhy <daytripper1989@gmail.com>
2016-05-28 01:27:23 +0200

    unspreads the code

--- cybersource_payment/core/models.py
+++ cybersource_payment/core/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
-
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+class Confirmation(models.Model):
+    access_key  = models.CharField(_('Access Key'),max_length=255,null=False,blank=False)
+    profile_id = models.CharField(_('Profile ID'),max_length=255,null=False,blank=False)
+    transaction_uuid = models.CharField(_('Transaction UUID'),max_length=255,null=False,blank=False)
+    signed_field_names = models.TextField(_('Signed Field Names'),max_length=255,null=False,blank=False)
+    unsigned_field_names = models.TextField(_('Unsigned Field Names'),max_length=255,null=True,blank=True)
+    signed_date_time = models.CharField(_('Signed Datetime'),max_length=255,null=False,blank=False)
+    locale = models.CharField(_('Locale'),max_length=255,null=False,blank=False)
+    transaction_type = models.CharField(_('Transaction Type'),max_length=255,null=False,blank=False)
+    #bill_address1 = models.CharField(_('Bill Address 1'),max_length=255,null=False,blank=False)
+    #bill_city = models.CharField(_('Bill City'),max_length=255,null=False,blank=False)
+    #bill_country = models.CharField(_('Bill Country'),max_length=255,null=False,blank=False)
+    #customer_email = models.CharField(_('Customer Email'),max_length=255,null=False,blank=False)
+    #customer_lastname = models.CharField(_('Customer Last Name'),max_length=255,null=False,blank=False)
+    reference_number = models.CharField(_('Reference Number'),max_length=255,null=False,blank=False)
+    amount = models.CharField(_('Amount'),max_length=255,null=False,blank=False)
+    currency = models.CharField(_('Currency'),max_length=255,null=False,blank=False)
+    signature = models.CharField(_('Signature'),max_length=255,null=False,blank=False)
+    ignore_avs = models.CharField(_('Ignore AVS'),max_length=255,null=False,blank=False)
--- cybersource_payment/core/views.py
+++ cybersource_payment/core/views.py
@@ -3,77 +3,87 @@ from django.template import RequestContext
 from models import *
 from forms import *
 from django.views.decorators.csrf import csrf_exempt
-import datetime
-import uuid
 import hmac
 import base64
 import hashlib
 
-secretKey = "b16669f97a9c4e4ab397cb8f75bc0765fdd99ca1c75445259d8947424dc83cd04fa04e0187424247b58895e22f1053cab9ce3d8b0b9b48f2bd357a5496c78f6c2e5c0c8e96c44603b8391a214aa046a06db5f41ecbbc4ba1ac1af5061f94a5e3f15db714e8c1496caa849f3b7b6dbf487f2ff4d9d3b84bac8bbb95f15f3e4180"
+from configuration import secretKey
 
 # Create your views here.
 
 
 def home(request):
+    #The view for home page
     pageTitle = "Home"
     return render_to_response('home.html', {'pageTitle': pageTitle}, context_instance=RequestContext(request))
 
 
 def pay(request):
-    from configuration import *
+    #The view for submitting payment data
     pageTitle = "Payment"
     paymentForm = PaymentForm()
-    uuID = str(uuid.uuid1())  # .replace("-", "")
-    utcDateTime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-    return render_to_response('pay.html', {'pageTitle': pageTitle, 'paymentForm': paymentForm, 'uuID': uuID, 'utcDateTime': utcDateTime, 'profileID': profileID, 'accessKey': accessKey}, context_instance=RequestContext(request))
+    return render_to_response('pay.html', {'pageTitle': pageTitle, 'paymentForm': paymentForm}, context_instance=RequestContext(request))
 
 
 def confirm(request):
+    #The view for payment confirmation
     pageTitle = "Confirmation"
     if request.method == 'POST':
         paymentForm = PaymentForm(request.POST)
         if paymentForm.is_valid():
-            paramsDict = {}
-            paramsDict['access_key'] = request.POST['access_key']
-            paramsDict['profile_id'] = request.POST['profile_id']
-            paramsDict['transaction_uuid'] = request.POST['transaction_uuid']
-            paramsDict['signed_field_names'] = request.POST['signed_field_names']
-            # paramsDict['unsigned_field_names'] = "payment_method,card_type,card_number,card_expiry_date,card_cvn,bill_to_forename,bill_to_surname,bill_to_email,bill_to_address_line1,bill_to_address_city,bill_to_address_postal_code,bill_to_address_state,bill_to_address_country"
-            # paramsDict['unsigned_field_names']="bill_to_forename,bill_to_surname,bill_to_email,bill_to_address_line1,bill_to_address_state,bill_to_address_country,payment_method,driver_license_state,driver_license_number,date_of_birth,echeck_account_type,company_tax_id,echeck_sec_code,echeck_account_number,echeck_routing_number"
-            paramsDict['unsigned_field_names'] = request.POST['unsigned_field_names']
-            paramsDict['signed_date_time'] = request.POST['signed_date_time']
-            paramsDict['locale'] = request.POST['locale']
-            paramsDict['transaction_type'] = paymentForm.cleaned_data[
-                'transaction_type']
-            paramsDict['reference_number'] = paymentForm.cleaned_data[
-                'reference_number']
-            paramsDict['amount'] = paymentForm.cleaned_data['amount']
-            paramsDict['currency'] = paymentForm.cleaned_data['currency']
-            keys = paramsDict['signed_field_names'].split(',')
-            message = ','.join(['{key}={value}'.format(
-                key=key, value=paramsDict[key]) for key in keys])
-            messageBeforeEncoding = message
-            message = message.encode('utf-8')
-            secret = secretKey.encode('utf-8')
-            signature = base64.b64encode(
-                hmac.new(secret, msg=message, digestmod=hashlib.sha256).digest()).decode()
-            return render_to_response('confirm.html', {'pageTitle': pageTitle, 'paymentForm': paymentForm, 'paramsDict': paramsDict, 'messageBeforeEncoding': messageBeforeEncoding, 'signature': signature}, context_instance=RequestContext(request))
+            paramsDict = getPostedParameters(paymentForm)
+            signature = sign(paramsDict)
+            paramsDict['signature'] = signature
+            confirmationForm = ConfirmationForm(paramsDict)
+            return render_to_response('confirm.html', {'pageTitle': pageTitle, 'confirmationForm': confirmationForm, 'paramsDict': paramsDict, 'signature': signature}, context_instance=RequestContext(request))
         else:
             from configuration import *
             pageTitle = "Payment"
-            #paymentForm = PaymentForm()
-            uuID = str(uuid.uuid1()).replace("-", "")
-            utcDateTime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-            return render_to_response('pay.html', {'pageTitle': pageTitle, 'paymentForm': paymentForm, 'uuID': uuID, 'utcDateTime': utcDateTime, 'profileID': profileID, 'accessKey': accessKey}, context_instance=RequestContext(request))
+            return render_to_response('pay.html', {'pageTitle': pageTitle, 'paymentForm': paymentForm }, context_instance=RequestContext(request))
     else:
         return pay(request)
 
 @csrf_exempt
 def receive(request):
+    #The view containing the receipt
     pageTitle = "Receipt"
     if request.method == 'POST':
-        d = request.POST
-        amount = d['req_amount']
-        return render_to_response('receive.html', {'pageTitle':pageTitle,'d':d,'amount':amount}, context_instance=RequestContext(request))
+        posted = request.POST
+        amount = posted['req_amount']
+        return render_to_response('receive.html', {'pageTitle':pageTitle,'posted':posted,'amount':amount}, context_instance=RequestContext(request))
     else:
         return pay(request)
+
+
+def sign(paramsDict):
+    #creates and returns the signature
+    keys = paramsDict['signed_field_names'].split(',')
+    message = ','.join(['{key}={value}'.format(key=key, value=paramsDict[key]) for key in keys])
+    messageBeforeEncoding = message
+    message = message.encode('utf-8')
+    secret = secretKey.encode('utf-8')
+    digested = hmac.new(secret, msg=message, digestmod=hashlib.sha256).digest()
+    signature = base64.b64encode(digested)#.decode()
+    return signature
+
+def getPostedParameters(paymentForm):
+    #places posted parameters in a dictionary and returns the dictionary
+    paramsDict = {}
+    paramsDict['access_key'] = paymentForm.cleaned_data['access_key']
+    paramsDict['profile_id'] = paymentForm.cleaned_data['profile_id']
+    paramsDict['transaction_uuid'] = paymentForm.cleaned_data['transaction_uuid']
+    paramsDict['signed_field_names'] = paymentForm.cleaned_data['signed_field_names']
+    paramsDict['unsigned_field_names'] = paymentForm.cleaned_data['unsigned_field_names']
+    paramsDict['signed_date_time'] = paymentForm.cleaned_data['signed_date_time']
+    paramsDict['locale'] = paymentForm.cleaned_data['locale']
+    paramsDict['transaction_type'] = paymentForm.cleaned_data['transaction_type']
+    #paramsDict['bill_address1'] = paymentForm.cleaned_data['bill_address1']
+    #paramsDict['bill_city'] = paymentForm.cleaned_data['bill_city']
+    #paramsDict['bill_country'] = paymentForm.cleaned_data['bill_country']
+    #paramsDict['customer_email'] = paymentForm.cleaned_data['customer_email']
+    #paramsDict['customer_lastname'] = paymentForm.cleaned_data['customer_lastname']
+    paramsDict['reference_number'] = paymentForm.cleaned_data['reference_number']
+    paramsDict['amount'] = paymentForm.cleaned_data['amount']
+    paramsDict['currency'] = paymentForm.cleaned_data['currency']
+    paramsDict['ignore_avs'] = paymentForm.cleaned_data['ignore_avs']
+    return paramsDict
